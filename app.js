c4ad05f014f5520efeb2ceff33f4cff8ccfdc778
nala-he <hytdaisy@gmail.com>
2022-11-21 14:07:16 -0500

    finished a9

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from "mongoose";
-mongoose.connect('mongodb://localhost:27017/tuiter');
-
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
@@ -10,6 +8,9 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/webdev';
+mongoose.connect(CONNECTION_STRING);
 
 HelloController(app);
 UserController(app);
