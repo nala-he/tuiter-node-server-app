4ba2d14923e8547bc34e03dab04fab2801f2a24b
nala-he <hytdaisy@gmail.com>
2022-11-21 09:01:36 -0500

    added mongoose to app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from "mongoose";
+mongoose.connect('mongodb://localhost:27017/tuiter');
 
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
